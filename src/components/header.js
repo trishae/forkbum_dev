5e21bd18e090893b791724e2a199df9ad30d3fea
Trisha Echual <piechual@gmail.com>
2020-09-09 01:22:15 -0400

    Add navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,23 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <Container>
+      <Navbar expand="md">
+        <Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarResponsive" />
+        <Navbar.Collapse id="navbarResponsive">
+          <Nav as="ul" className="ml-auto">
+            <Nav.Item as="li">
+              <Link to="/page-2" className="nav-link" activeClassName="active"></Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
   </header>
 )
 
