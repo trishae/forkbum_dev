5a8123c74356fdf73f3b03cb67c3940015fcf037
Trisha Echual <piechual@gmail.com>
2020-09-09 20:40:05 -0400

    Fix minor issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Container } from 'react-bootstrap'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
