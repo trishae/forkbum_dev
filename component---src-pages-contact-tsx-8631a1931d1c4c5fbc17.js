610b0b10f02001b3085528b72ddd4635fb50594f
trishae <trishae@protonmail.com>
2021-09-02 15:48:11 -0700

    Updates

--- component---src-pages-contact-tsx-8631a1931d1c4c5fbc17.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"0Aie":function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),c=t.n(n),o=t("7vrA"),l=t("3Z9Z"),r=t("Bl7J"),s=t("vrFN");t("OooO");a.default=function(e){return c.a.createElement(r.a,null,c.a.createElement(s.a,{title:"Contact"}),c.a.createElement(o.a,{className:"page-frame"},c.a.createElement(l.a,{className:"page-heading"},"contact"),c.a.createElement(l.a,{className:"page-body"},"contact")))}},OooO:function(e,a,t){}}]);
-//# sourceMappingURL=component---src-pages-contact-tsx-8631a1931d1c4c5fbc17.js.map
